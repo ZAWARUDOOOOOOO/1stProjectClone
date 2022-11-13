de27cb4cf4cf22f0537091508e192f7eef438002
Saveliy <ihavelostmoreforless@yandex.ru>
2022-11-13 22:06:47 +0300

    FinanceExchangePage class added to the project.

---
+++ src/test/java/com/example/demo/pages/FinanceExchangePage.java
@@ -0,0 +1,25 @@
+package com.example.demo.pages;
+
+import com.example.demo.AbstractClasses.BaseSeleniumPage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FinanceExchangePage extends BaseSeleniumPage {
+
+    @FindBy(xpath = "(//*[contains(@class, \"css-1kz2xbf\")])[1]")
+    private WebElement usdToRubInfoField;
+
+    @FindBy(xpath = "(//*[contains(@class, \"css-1kz2xbf\")])[3]")
+    private WebElement rubToUsdInfoField;
+
+    public FinanceExchangePage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public Boolean isConverterDisplayed() {
+        Boolean isUsdToRubInfoDisplayed = usdToRubInfoField.isDisplayed();
+        Boolean isRubToUsdInfoDisplayed = rubToUsdInfoField.isDisplayed();
+        return isUsdToRubInfoDisplayed & isRubToUsdInfoDisplayed;
+    }
+}
